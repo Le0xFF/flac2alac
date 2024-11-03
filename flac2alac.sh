1f7b65f306cb697b92a32eba6457b5a69d0eb000
Le0xFF <43521247+Le0xFF@users.noreply.github.com>
2024-11-03 21:11:54 +0100

    Beautify the script to suit my own taste

--- flac2alac.sh
+++ flac2alac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# flac2alac By Arlindo \"Nighto\" Pereira <nighto@nighto.net>
+# flac2alac By Arlindo "Nighto" Pereira <nighto@nighto.net>
 # (C) 2010. Licensed under GPLv3
 # Modified by Jeffrey Paul <sneak@datavibe.net>
 # Inspired by MetalMusicAddict's script
@@ -16,9 +16,9 @@ function usage() {
     echo ""
     echo "Be sure to install imagemagick, flac, ffmpeg and file, as they are needed for the script."
     echo ""
-    echo "Usage: ${0} [-r] <INPUT_FLAC>"
+    echo "Usage: ${0} [-d] <INPUT_FLAC>"
     echo ""
-    echo -e "\t-r\t\tRemove the original FLAC file after successful conversion."
+    echo -e "\t-d\t\tRemove the original FLAC file after successful conversion."
     echo ""
     echo -e "\tINPUT_FLAC\tPath to the FLAC file or files to be converted."
     echo -e "\t\t\tSupports wildcard *.flac for batch conversion."
@@ -27,81 +27,89 @@ function usage() {
 }
 
 # Check for no arguments
-if [ "$#" -eq 0 ]; then
+if [ "${#}" -eq 0 ]
+then
     usage
     exit 1
 fi
 
 # Check for help option
-if [ "$1" == "-h" ]; then
+if [ "${1}" == "-h" ] || [ "${1}" == "--help" ]
+then
     usage
     exit 0
 fi
 
 function _convert_flac2alac {
     # Prepare file names and paths
-    local NF="`basename \"$1\" .flac`.m4a"
-    local D="`dirname \"$1\"`"
+    local NF="$(basename "${1}" .flac).m4a"
+    local D="$(dirname "${1}")"
 
     # Decode FLAC to temporary WAV file
-    flac -dc "$1" > "${D}/.flacdecode.${NF}.wav"
-    if [ $? -ne 0 ]; then
+    flac -dc "${1}" > "${D}/.flacdecode.${NF}.wav"
+    if [ "${?}" -ne 0 ]
+    then
         echo "ERROR: Corrupt or invalid FLAC file, exiting." >&2
         exit 1
     fi
 
     # Extract metadata using metaflac and sed to remove the tag prefix
-    local ARTIST="`metaflac --show-tag=ARTIST \"$1\" | sed s/ARTIST=//ig`"
-    local ALBUMARTIST="`metaflac --show-tag=ALBUMARTIST \"$1\" | sed s/ALBUMARTIST=//ig`"
-    local TITLE="`metaflac --show-tag=TITLE \"$1\" | sed s/TITLE=//ig`"
-    local ALBUM="`metaflac --show-tag=ALBUM \"$1\" | sed s/ALBUM=//ig`"
-    local DATE="`metaflac --show-tag=DATE \"$1\" | sed s/DATE=//ig`"
-    local GENRE="`metaflac --show-tag=GENRE \"$1\" | sed s/GENRE=//ig`"
-    local TRACKNUMBER="`metaflac --show-tag=TRACKNUMBER \"$1\" | sed s/TRACKNUMBER=//ig`"
-    local TRACKTOTAL="`metaflac --show-tag=TRACKTOTAL \"$1\" | sed s/TRACKTOTAL=//ig`"
-    local DISCNUMBER="`metaflac --show-tag=DISCNUMBER \"$1\" | sed s/DISCNUMBER=//ig`"
-    local DISCTOTAL="`metaflac --show-tag=DISCTOTAL \"$1\" | sed s/DISCTOTAL=//ig`"
-    local DESCRIPTION="`metaflac --show-tag=DESCRIPTION \"$1\" | sed s/DESCRIPTION=//ig`"
-    local COMPOSER="`metaflac --show-tag=COMPOSER \"$1\" | sed s/COMPOSER=//ig`"
-    local LYRICS="$(metaflac --show-tag=LYRICS "${1}" | sed s/LYRICS=//ig)"
+    local ARTIST="$(metaflac --show-tag=ARTIST "${1}"           | sed s/ARTIST=//ig)"
+    local ALBUMARTIST="$(metaflac --show-tag=ALBUMARTIST "${1}" | sed s/ALBUMARTIST=//ig)"
+    local TITLE="$(metaflac --show-tag=TITLE "${1}"             | sed s/TITLE=//ig)"
+    local ALBUM="$(metaflac --show-tag=ALBUM "${1}"             | sed s/ALBUM=//ig)"
+    local DATE="$(metaflac --show-tag=DATE "${1}"               | sed s/DATE=//ig)"
+    local GENRE="$(metaflac --show-tag=GENRE "${1}"             | sed s/GENRE=//ig)"
+    local TRACKNUMBER="$(metaflac --show-tag=TRACKNUMBER "${1}" | sed s/TRACKNUMBER=//ig)"
+    local TRACKTOTAL="$(metaflac --show-tag=TRACKTOTAL "${1}"   | sed s/TRACKTOTAL=//ig)"
+    local DISCNUMBER="$(metaflac --show-tag=DISCNUMBER "${1}"   | sed s/DISCNUMBER=//ig)"
+    local DISCTOTAL="$(metaflac --show-tag=DISCTOTAL "${1}"     | sed s/DISCTOTAL=//ig)"
+    local DESCRIPTION="$(metaflac --show-tag=DESCRIPTION "${1}" | sed s/DESCRIPTION=//ig)"
+    local COMPOSER="$(metaflac --show-tag=COMPOSER "${1}"       | sed s/COMPOSER=//ig)"
+    local LYRICS="$(metaflac --show-tag=LYRICS "${1}"           | sed s/LYRICS=//ig)"
 
     # Determine the MIME type of the embedded picture
-	local ARTFILE=".arttmp.${NF}"
+    local ARTFILE=".arttmp.${NF}"
     
-	# Extracting the MIME type of the embedded picture to determine its format
-	local ARTFORMAT=$(metaflac --export-picture-to=- "$1" | file -i -b - | awk '{split($1,a,";"); print a[1]}')
-	echo "ARTFORMAT: $ARTFORMAT"
-	echo "Arttext : $ARTEXT"
+    # Extracting the MIME type of the embedded picture to determine its format
+    local ARTFORMAT=$(metaflac --export-picture-to=- "${1}" \
+                        | file -i -b - \
+                        | awk '{split($1,a,";"); print a[1]}')
+    echo "ARTFORMAT : ${ARTFORMAT}"
+    echo "ARTEXT    : ${ARTEXT}"
 
-	# THIS PART WILL BE REWRITTEN
+    # THIS PART WILL BE REWRITTEN
     # CHECK ARTFORMAT, MAKE LOWERCASE, SET ARTFILE TO IT
 
-	# Proceed with the rest of the script based on ARTFORMAT's value
-	if [ "$ARTFORMAT" != "application/x-empty" ]; then
-		echo "ARTFILE : $ARTFILE"
-		echo "Arttext: $ARTEXT"
+    # Proceed with the rest of the script based on ARTFORMAT's value
+    if [ "${ARTFORMAT}" != "application/x-empty" ]
+    then
+        echo "ARTFILE : ${ARTFILE}"
+        echo "ARTEXT  : ${ARTEXT}"
 
-		if [ "$ARTFORMAT" = "image/png" ]; then ARTEXT="png"; fi
-		if [ "$ARTFORMAT" = "image/jpeg" ]; then ARTEXT="jpg"; fi
-		if [ "$ARTFORMAT" = "JPEG" ]; then ARTEXT="jpg"; fi
-		if [ -z "$ARTEXT" ]; then
-			ARTFILE=$(find . -type f \( -iname "*.jpg" -o -iname "*.png" \) -print -quit)
-			echo "Directory artfile : $ARTFILE"
-			if [ -n "$ARTFILE" ]; then
-    			echo "Found file: $ARTFILE"
-				
-			else
-    			echo "No .jpg or .png files found."
-				echo "Unknown embedded album art format: ${ARTFORMAT}, cannot continue." >&2
-				exit 1
-			fi
-			
-		else
-			ARTFILE=".arttmp.${NF}.${ARTEXT}"  # Ensure the file extension is appended
-			metaflac --export-picture-to="${D}/${ARTFILE}" "$1"
-		fi
-		echo "ARTFILE : $ARTFILE"
-		echo "Arttext: $ARTEXT"
+        if [ "${ARTFORMAT}" = "image/png" ]; then ARTEXT="png"; fi
+        if [ "${ARTFORMAT}" = "image/jpeg" ]; then ARTEXT="jpg"; fi
+        if [ "${ARTFORMAT}" = "JPEG" ]; then ARTEXT="jpg"; fi
+
+        if [ -z "${ARTEXT}" ]
+        then
+            ARTFILE=$(find . -type f \( -iname "*.jpg" -o -iname "*.png" \) -print -quit)
+            echo "Directory artfile : ${ARTFILE}"
+            if [ -n "${ARTFILE}" ]
+            then
+                echo "Found file: ${ARTFILE}"
+            else
+                echo "No .jpg or .png files found."
+                echo "Unknown embedded album art format: ${ARTFORMAT}, cannot continue." >&2
+                exit 1
+            fi
+        else
+            ARTFILE=".arttmp.${NF}.${ARTEXT}"  # Ensure the file extension is appended
+            metaflac --export-picture-to="${D}/${ARTFILE}" "${1}"
+        fi
+
+        echo "ARTFILE : ${ARTFILE}"
+        echo "ARTEXT  : ${ARTEXT}"
 
         # Resize ARTFILE to 500x500 pixel , convert to jpg
         # and save it as non progressive jpg, to be compatible with RockBox
@@ -112,28 +120,28 @@ function _convert_flac2alac {
         else
             convert "${D}/${ARTFILE}" -resize 500x500 -interlace none "${D}/${ARTFILE}"
         fi
-
-	fi
+    fi
 
     ffmpeg -hide_banner -i "${D}/.flacdecode.${NF}.wav" \
-    ${ARTFILE:+-i "${D}/${ARTFILE}"} \
-    -map 0:a ${ARTFILE:+-map 1:v} \
-    -metadata title="$TITLE" \
-    -metadata artist="$ARTIST" \
-    -metadata album_artist="$ALBUMARTIST" \
-    -metadata album="$ALBUM" \
-    -metadata genre="$GENRE" \
-    -metadata date="$DATE" \
-    -metadata track="$TRACKNUMBER/$TRACKTOTAL" \
-    -metadata disc="$DISCNUMBER/$DISCTOTAL" \
-    -metadata composer="$COMPOSER" \
-    -metadata comment="$DESCRIPTION" \
-    -metadata lyrics="${LYRICS}" \
-    ${ARTFILE:+-disposition:v:0 attached_pic} \
-    -c:a alac ${ARTFILE:+-c:v copy} \
-    "${D}/.tmp.${NF}"
-    
-    if [ $? -ne 0 ]; then
+        ${ARTFILE:+-i "${D}/${ARTFILE}"} \
+        -map 0:a ${ARTFILE:+-map 1:v} \
+        -metadata title="${TITLE}" \
+        -metadata artist="${ARTIST}" \
+        -metadata album_artist="${ALBUMARTIST}" \
+        -metadata album="${ALBUM}" \
+        -metadata genre="${GENRE}" \
+        -metadata date="${DATE}" \
+        -metadata track="${TRACKNUMBER}/${TRACKTOTAL}" \
+        -metadata disc="${DISCNUMBER}/${DISCTOTAL}" \
+        -metadata composer="${COMPOSER}" \
+        -metadata comment="${DESCRIPTION}" \
+        -metadata lyrics="${LYRICS}" \
+        ${ARTFILE:+-disposition:v:0 attached_pic} \
+        -c:a alac ${ARTFILE:+-c:v copy} \
+        "${D}/.tmp.${NF}"
+
+    if [ $? -ne 0 ]
+    then
         echo "Problem running conversion, exiting." >&2
         exit 1
     fi
@@ -146,8 +154,9 @@ function _convert_flac2alac {
     rm -f "${D}/.flacdecode.${NF}.wav"
 
     # Option to delete original FLAC file after successful conversion
-    if [ $DELETE_WHEN_DONE -eq 1 ]; then
-        rm "$1"
+    if [ "${DELETE_WHEN_DONE}" -eq 1 ]
+    then
+        rm "${1}"
         echo "Original FLAC file deleted."
     fi
 }
@@ -155,17 +164,21 @@ function _convert_flac2alac {
 # Main script logic: parse arguments and call conversion function
 DELETE_WHEN_DONE=0
 
-for filename in "$@"; do
-    if [ "$filename" = "-d" ]; then
+for filename in "${@}"
+do
+    if [ "${filename}" = "-d" ]
+    then
         DELETE_WHEN_DONE=1
-    elif [ -d "$filename" ]; do
+    elif [ -d "${filename}" ]
+    then
         IFS=$'\n'
-        for one_filename in $(find "$filename" -type f -iname "*.flac"); do
+        for one_filename in $(find "${filename}" -type f -iname "*.flac")
+        do
             unset IFS
-            _convert_flac2alac "$one_filename"
+            _convert_flac2alac "${one_filename}"
         done
-    elif [ -f "$filename" ]; then
-        _convert_flac2alac "$filename"
+    elif [ -f "${filename}" ]
+    then
+        _convert_flac2alac "${filename}"
     fi
 done
-
